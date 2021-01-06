c72716d64726e41fe4ee12aa2355bf5d3ab6c244
thibaudk <tibthejazzdude@gmail.com>
2021-01-06 02:51:35 +0100

    up ValueFilter

--- src/plugins/score-plugin-fx/Fx/NoiseFilter.hpp
+++ src/plugins/score-plugin-fx/Fx/NoiseFilter.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include "1efilter.hpp"
 #include <Engine/Node/Process.hpp>
+#include <dno/src/DeNoiser.hpp>
 
 struct NoiseFilter
 {
@@ -13,11 +13,11 @@ struct NoiseFilter
 
           ossia::value operator()(int i) const
           {
-            return nFilt->one_euro_i(i, 1);
+            return nFilt->dno_i(i);
           }
           ossia::value operator()(float f) const
           {
-            return nFilt->one_euro_f(f, 1);
+            return nFilt->dno_f(f);
           }
           ossia::value operator()(bool b) const
           {
@@ -74,32 +74,32 @@ struct NoiseFilter
       return val;
     }
 
-    one_euro_filter<int> one_euro_i;
-    one_euro_filter<float> one_euro_f;
-    one_euro_filter<ossia::vec2f> one_euro_v2;
-    one_euro_filter<ossia::vec3f> one_euro_v3;
-    one_euro_filter<ossia::vec4f> one_euro_v4;
-    one_euro_filter<std::vector<ossia::value>> one_euro_v;
+    DeNoiser<int> dno_i;
+    DeNoiser<float> dno_f;
+    DeNoiser<ossia::vec2f> dno_v2;
+    DeNoiser<ossia::vec3f> dno_v3;
+    DeNoiser<ossia::vec4f> dno_v4;
+    DeNoiser<std::vector<ossia::value>> dno_v;
 
     NoiseFilter(const float frequency = 120,
                 const float minCutoff = 1,
                 const float beta = 1,
                 const float dCutoff = 1)
-      : one_euro_i{one_euro_filter<int>(frequency, minCutoff, beta, dCutoff)},
-        one_euro_f{one_euro_filter<float>(frequency, minCutoff, beta, dCutoff)},
-        one_euro_v2{one_euro_filter<ossia::vec2f>(frequency,
+      : dno_i{DeNoiser<int>(frequency, minCutoff, beta, dCutoff)},
+        dno_f{DeNoiser<float>(frequency, minCutoff, beta, dCutoff)},
+        dno_v2{DeNoiser<ossia::vec2f>(frequency,
                                                   ossia::vec2f{minCutoff},
                                                   ossia::vec2f{beta},
                                                   ossia::vec2f{dCutoff})},
-        one_euro_v3{one_euro_filter<ossia::vec3f>(frequency,
+        dno_v3{DeNoiser<ossia::vec3f>(frequency,
                                                   ossia::vec3f{minCutoff},
                                                   ossia::vec3f{beta},
                                                   ossia::vec3f{dCutoff})},
-        one_euro_v4{one_euro_filter<ossia::vec4f>(frequency,
+        dno_v4{DeNoiser<ossia::vec4f>(frequency,
                                                   ossia::vec4f{minCutoff},
                                                   ossia::vec4f{beta},
                                                   ossia::vec4f{dCutoff})},
-        one_euro_v{one_euro_filter<std::vector<ossia::value>>(frequency,
+        dno_v{DeNoiser<std::vector<ossia::value>>(frequency,
                                                               std::vector<ossia::value>{minCutoff},
                                                               std::vector<ossia::value>{beta},
                                                               std::vector<ossia::value>{dCutoff})}
--- src/plugins/score-plugin-fx/Fx/ValueFilter.hpp
+++ src/plugins/score-plugin-fx/Fx/ValueFilter.hpp
@@ -1,127 +1,231 @@
 #pragma once
 #include <Engine/Node/PdNode.hpp>
-#include "NoiseFilter.hpp"
+#include <libossia/3rdparty/dno/src/DeNoiser.hpp>
 
 namespace Nodes::ValueFilter
 {
 struct Node
 {
-  struct Metadata : Control::Meta_base
-  {
-    static const constexpr auto prettyName = "Value filter";
-    static const constexpr auto objectKey = "ValueFilter";
-    static const constexpr auto category = "Mappings";
-    static const constexpr auto author = "ossia score";
-    static const constexpr auto tags = std::array<const char*, 0>{};
-    static const constexpr auto kind = Process::ProcessCategory::Mapping;
-    static const constexpr auto description = "Filter and limit noisy value stream";
-    static const uuid_constexpr auto uuid = make_uuid("809c014d-7d02-45dc-8849-de7a7db5fe67");
+    struct NoiseFilter
+    {
+        ossia::value filter(const ossia::value& val)
+        {
+          struct vis
+          {
+              ossia::value operator()() const { return {}; }
+              ossia::value operator()(const ossia::impulse&) const { return {}; }
 
-    static const constexpr auto controls = std::make_tuple(
-          Control::make_enum(
-            "Type",
-            0U,
-            ossia::make_array(
-              "1Euro",
-              "Exponential"
+              ossia::value operator()(int i) const
+              {
+                return nFilt->dno_i(i);
+              }
+              ossia::value operator()(float f) const
+              {
+                return nFilt->dno_f(f);
+              }
+              ossia::value operator()(bool b) const
+              {
+                return b;
+              }
+
+              ossia::value operator()(const std::string& s) const
+              {
+                return s;
+              }
+
+              ossia::value operator()(const ossia::vec2f& t) const
+              {
+                return t;
+              }
+
+              ossia::value operator()(const ossia::vec3f& t) const
+              {
+                return t;
+              }
+
+              ossia::value operator()(const ossia::vec4f& t) const
+              {
+                return t;
+              }
+
+              ossia::value operator()(const std::vector<ossia::value>& t) const
+              {
+                std::vector<ossia::value> res;
+
+                for (auto& v : t)
+                {
+                  res.push_back(ossia::apply(*this, v.v));
+                }
+
+                return res;
+              }
+
+              NoiseFilter* nFilt;
+
+              vis(NoiseFilter* enclosing)
+                : nFilt{enclosing}
+              {}
+          };
+
+          try {
+            return ossia::apply(vis{this}, val.v);
+          } catch(std::exception& e) {
+            std::cerr << e.what() << std::endl;
+          } catch(...) {
+            std::cerr << "error" << std::endl;
+          }
+
+          return val;
+        }
+
+        DeNoiser<int> dno_i;
+        DeNoiser<float> dno_f;
+        DeNoiser<ossia::vec2f> dno_v2;
+        DeNoiser<ossia::vec3f> dno_v3;
+        DeNoiser<ossia::vec4f> dno_v4;
+        DeNoiser<std::vector<ossia::value>> dno_v;
+
+        NoiseFilter(const float frequency = 120,
+                    const float minCutoff = 1,
+                    const float beta = 1,
+                    const float dCutoff = 1)
+          : dno_i{DeNoiser<int>(frequency, minCutoff, beta, dCutoff)},
+            dno_f{DeNoiser<float>(frequency, minCutoff, beta, dCutoff)},
+            dno_v2{DeNoiser<ossia::vec2f>(frequency,
+                                                      ossia::vec2f{minCutoff},
+                                                      ossia::vec2f{beta},
+                                                      ossia::vec2f{dCutoff})},
+            dno_v3{DeNoiser<ossia::vec3f>(frequency,
+                                                      ossia::vec3f{minCutoff},
+                                                      ossia::vec3f{beta},
+                                                      ossia::vec3f{dCutoff})},
+            dno_v4{DeNoiser<ossia::vec4f>(frequency,
+                                                      ossia::vec4f{minCutoff},
+                                                      ossia::vec4f{beta},
+                                                      ossia::vec4f{dCutoff})},
+            dno_v{DeNoiser<std::vector<ossia::value>>(frequency,
+                                                                  std::vector<ossia::value>{minCutoff},
+                                                                  std::vector<ossia::value>{beta},
+                                                                  std::vector<ossia::value>{dCutoff})}
+        {}
+    };
+
+        struct Metadata : Control::Meta_base
+        {
+            static const constexpr auto prettyName = "Value filter";
+            static const constexpr auto objectKey = "ValueFilter";
+            static const constexpr auto category = "Mappings";
+            static const constexpr auto author = "ossia score";
+            static const constexpr auto tags = std::array<const char*, 0>{};
+            static const constexpr auto kind = Process::ProcessCategory::Mapping;
+            static const constexpr auto description = "Filter and limit noisy value stream";
+            static const uuid_constexpr auto uuid = make_uuid("809c014d-7d02-45dc-8849-de7a7db5fe67");
+
+            static const constexpr auto controls = std::make_tuple(
+                  Control::make_enum(
+                    "Type",
+                    0U,
+                    ossia::make_array(
+                      "1Euro",
+                      "Exp"
               "Average",
-              "None")),
-          Control::FloatSlider{"amount", 0., 1., .5},
-          Control::Toggle{"rate", false},
-          Control::IntSpinBox{"ms.", 0, 100, 0},
-          Control::Widgets::QuantificationChooser());
+                      "None")),
+                  Control::FloatSlider{"amount", 0., 1., .5},
+                  Control::Toggle{"rate", false},
+                  Control::IntSpinBox{"ms.", 0, 100, 0},
+                  Control::Widgets::QuantificationChooser());
 
-    static const constexpr value_in value_ins[]{"in"};
-    static const constexpr value_out value_outs[]{"out"};
-  };
+            static const constexpr value_in value_ins[]{"in"};
+            static const constexpr value_out value_outs[]{"out"};
+        };
 
-  struct State
-  {
-      NoiseFilter nf{};
+        struct State
+        {
+            NoiseFilter nf{};
 
-      ossia::value smooth(const ossia::value& value, const std::string& smoothType, const float& amount)
-      {
-        if (smoothType == "1Euro")
-          return nf.filter(value);
-        else if (smoothType == "Average");
-      }
-  };
+            ossia::value smooth(const ossia::value& value, const std::string& smoothType, const float& amount)
+            {
+              if (smoothType == "1Euro")
+                return nf.filter(value);
+              else if (smoothType == "Average");
+            }
+        };
 
-  using control_policy = ossia::safe_nodes::last_tick;
+        using control_policy = ossia::safe_nodes::last_tick;
 
-  static void
-  run(const ossia::value_port& in,
-      const std::string& type,
-      float amount,
-      bool rate,
-      int ms,
-      float quantif,
-      ossia::value_port& out,
-      ossia::token_request t,
-      ossia::exec_state_facade st,
-      State& self)
-  {
-    for (const ossia::timed_value& v : in.get_data())
-    { 
-      if (rate)
-      {
-      }
-      else
-      {
-        out.write_value(self.smooth(v.value, type, amount), v.timestamp);
-      }
-    }
-  }
+        static void
+        run(const ossia::value_port& in,
+            const std::string& type,
+            float amount,
+            bool rate,
+            int ms,
+            float quantif,
+            ossia::value_port& out,
+            ossia::token_request t,
+            ossia::exec_state_facade st,
+            State& self)
+        {
+          for (const ossia::timed_value& v : in.get_data())
+          {
+            if (rate)
+            {
+            }
+            else
+            {
+              out.write_value(self.smooth(v.value, type, amount), v.timestamp);
+            }
+          }
+        }
 
-  static void item(
-      Process::Enum& type,
-      Process::FloatSlider& amount,
-      Process::Toggle& rate,
-      Process::IntSpinBox& ms,
-      Process::ComboBox& quantif,
-      const Process::ProcessModel& process,
-      QGraphicsItem& parent,
-      QObject& context,
-      const Process::Context& doc)
-  {
-    using namespace Process;
-    const Process::PortFactoryList& portFactory = doc.app.interfaces<Process::PortFactoryList>();
-    const auto tMarg = 10;
-    const auto cMarg = 15;
-    const auto h = 85;
-    const auto w = 220;
+        static void item(
+            Process::Enum& type,
+            Process::FloatSlider& amount,
+            Process::Toggle& rate,
+            Process::IntSpinBox& ms,
+            Process::ComboBox& quantif,
+            const Process::ProcessModel& process,
+            QGraphicsItem& parent,
+            QObject& context,
+            const Process::Context& doc)
+        {
+          using namespace Process;
+          const Process::PortFactoryList& portFactory = doc.app.interfaces<Process::PortFactoryList>();
+          const auto tMarg = 10;
+          const auto cMarg = 15;
+          const auto h = 85;
+          const auto w = 220;
 
-    auto c0_bg = new score::BackgroundItem{&parent};
-    c0_bg->setRect({0., 0., w, h});
-    auto c1_bg = new score::BackgroundItem{&parent};
-    c1_bg->setRect({0., h, w, 45.});
+          auto c0_bg = new score::BackgroundItem{&parent};
+          c0_bg->setRect({0., 0., w, h});
+          auto c1_bg = new score::BackgroundItem{&parent};
+          c1_bg->setRect({0., h, w, 45.});
 
-    auto type_item = makeControl(
-          std::get<0>(Metadata::controls), type, parent, context, doc, portFactory);
-    type_item.root.setPos(tMarg, 0);
-    type_item.control.rows = 1;
-    type_item.control.columns = 3;
-    type_item.control.setRect(QRectF{0, 0, 200, 25});
+          auto type_item = makeControl(
+                std::get<0>(Metadata::controls), type, parent, context, doc, portFactory);
+          type_item.root.setPos(tMarg, 0);
+          type_item.control.rows = 1;
+          type_item.control.columns = 4;
+          type_item.control.setRect(QRectF{0, 0, 200, 25});
 
-    auto amount_item = makeControl(
-        std::get<1>(Metadata::controls), amount, parent, context, doc, portFactory);
-    amount_item.root.setPos(tMarg, 40);
-    amount_item.control.setPos(0, cMarg);
+          auto amount_item = makeControl(
+                std::get<1>(Metadata::controls), amount, parent, context, doc, portFactory);
+          amount_item.root.setPos(tMarg, 40);
+          amount_item.control.setPos(0, cMarg);
 
-    auto rate_item = makeControl(
-          std::get<2>(Metadata::controls), rate, parent, context, doc, portFactory);
-    rate_item.root.setPos(tMarg, h);
-    rate_item.control.setPos(0, tMarg);
+          auto rate_item = makeControl(
+                std::get<2>(Metadata::controls), rate, parent, context, doc, portFactory);
+          rate_item.root.setPos(tMarg, h);
+          rate_item.control.setPos(0, tMarg);
 
-    auto ms_item = makeControl(
-          std::get<3>(Metadata::controls), ms, parent, context, doc, portFactory);
-    ms_item.root.setPos(55, h);
-    ms_item.control.setPos(0, cMarg);
+          auto ms_item = makeControl(
+                std::get<3>(Metadata::controls), ms, parent, context, doc, portFactory);
+          ms_item.root.setPos(55, h);
+          ms_item.control.setPos(0, cMarg);
 
-    auto quant_item = makeControl(
-          std::get<4>(Metadata::controls), quantif, parent, context, doc, portFactory);
-    quant_item.root.setPos(130, h);
-    quant_item.control.setPos(cMarg, cMarg);
-  }
-};
+          auto quant_item = makeControl(
+                std::get<4>(Metadata::controls), quantif, parent, context, doc, portFactory);
+          quant_item.root.setPos(130, h);
+          quant_item.control.setPos(cMarg, cMarg);
+        }
+    };
 }
